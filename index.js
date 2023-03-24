b873fc464759199753d11352b9fd73537d0076ba
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-03-24 01:15:36 -0500

    integrated openai api

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/openai', require('./endpoints/openai_endpoints'));
 app.listen(port, () => console.log("Server started on port ${port}"));
 
