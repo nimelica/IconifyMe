c3003e40f0d5021b395152047e472bed4d129f80
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-03-25 01:07:06 -0500

    add body parser

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/openai', require('./endpoints/openai_endpoints'));
 app.listen(port, () => console.log("Server started on port ${port}"));
 
