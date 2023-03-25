d87ad702f4add92d1b618bda5748ccbb5ca2ec52
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-03-24 23:35:11 -0500

    update contollers

--- controllers/openai_controller.js
+++ controllers/openai_controller.js
@@ -1,14 +1,43 @@
-const { Configuration, OpenAIApi } = require("openai");
+//for more details: https://platform.openai.com/docs/guides/images/usage
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-    res.status(200).json({
-       success: true,
+  //const { prompt, size } = req.body;
+
+  //const imageSize =
+    //size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
+  try {
+    const response = await openai.createImage({
+      prompt: "mona lisa",
+      n: 1,
+      size: "512x512",
     });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+  }
 };
 
-module.exports = { generateImage }
+module.exports = { generateImage };
--- endpoints/openai_endpoints.js
+++ endpoints/openai_endpoints.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { generateImage } = require('..controllers/openaoi_contoller')
+const { generateImage } = require('../controllers/openai_controller');
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
