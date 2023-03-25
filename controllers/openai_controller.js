4b4e3bf3a18d24fbda4620194ecad33088179fba
Nimet Ozkan <nimozkan@Nimets-MacBook-Pro.local>
2023-03-25 01:06:24 -0500

    parse prompt

--- controllers/openai_controller.js
+++ controllers/openai_controller.js
@@ -7,23 +7,29 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
-  //const { prompt, size } = req.body;
+  const { prompt, size } = req.body || {};
 
-  //const imageSize =
-    //size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+  if (!prompt) {
+    return res.status(400).json({
+      success: false,
+      error: "'prompt' is a required property",
+    });
+  }
+
+  const iconSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
   try {
     const response = await openai.createImage({
-      prompt: "mona lisa",
+      prompt,
       n: 1,
-      size: "512x512",
+      size: iconSize,
     });
 
-    const imageUrl = response.data.data[0].url;
+    const iconUrl = response.data.data[0].url;
 
     res.status(200).json({
       success: true,
-      data: imageUrl,
+      data: iconUrl,
     });
   } catch (error) {
     if (error.response) {
